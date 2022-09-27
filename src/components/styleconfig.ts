c3ed42c72bfd3a03ca6a0365d9040bb512bff388
ClemMoreau <clement.moreau92@univ-tours.fr>
2022-09-27 08:57:23 +0200

    add button & default style file

---
+++ src/components/styleconfig.ts
@@ -0,0 +1,16 @@
+const styleConfig = {
+    primaryColor: "#00d167",
+    buttonSizes: {
+        sm: "15",
+        md: "155",
+        lg: "250",
+    },
+    inputSizes: {
+        padding_sm: "0.2rem !important",
+        minWidth_sm: "5rem !important",
+        padding_md: "1 rem",
+        minWidth_md: "40rem",
+    },
+};
+
+export default styleConfig;
